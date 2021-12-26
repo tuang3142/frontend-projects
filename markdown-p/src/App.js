a683937d030ca50d106980c316ca0c03e3464d33
Tuan Nguyen <tuan.nguyen@remitano.com>
2021-12-26 15:24:29 +0700

    Do not use React.createElement

--- markdown-p/src/App.js
+++ markdown-p/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { marked } from 'marked';
 import Prism from 'prismjs';
-import logo from './logo.svg';
 import placeholder from './placeholder';
-import './stylesheet/App.css';
+import './App.css';
 
-const projectName = 'markdown-previewer';
+const Toolbar = props => (
+  <div className="toolbar">
+    <i className="fa fa-free-code-camp" />
+    {props.text}
+    <i className={props.icon} onClick={props.onClick}/>
+  </div>
+);
 
-// ALLOWS LINE BREAKS WITH RETURN BUTTON
 marked.setOptions({
   breaks: true,
-  highlight: function (code) {
+  highlight: code => {
     return Prism.highlight(code, Prism.languages.javascript, 'javascript');
-  } });
+  }
+});
 
+const Preview = props => (
+  <div id="preview"
+    dangerouslySetInnerHTML={{
+      __html: marked(props.markdown, { renderer: new marked.Renderer() })
+    }}
+  />
+);
 
-// INSERTS target="_blank" INTO HREF TAGS (required for Codepen links)
-const renderer = new marked.Renderer();
-renderer.link = function (href, title, text) {
-  return `<a target="_blank" href="${href}">${text}</a>`;
-};
+const Editor = props => (
+  <textarea id="editor" onChange={props.onChange}
+    type="text" value={props.markdown} />
+);
 
 class App extends React.Component {
   constructor(props) {
@@ -28,88 +38,48 @@ class App extends React.Component {
     this.state = {
       markdown: placeholder,
       editorMaximized: false,
-      previewMaximized: false };
-
+      previewMaximized: false
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleEditorMaximize = this.handleEditorMaximize.bind(this);
     this.handlePreviewMaximize = this.handlePreviewMaximize.bind(this);
   }
+
   handleChange(e) {
     this.setState({
       markdown: e.target.value });
-
   }
+
   handleEditorMaximize() {
     this.setState({
       editorMaximized: !this.state.editorMaximized });
-
   }
+
   handlePreviewMaximize() {
     this.setState({
       previewMaximized: !this.state.previewMaximized });
-
   }
+
   render() {
-    const classes = this.state.editorMaximized ?
-    ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress'] :
-    this.state.previewMaximized ?
-    ['editorWrap hide', 'previewWrap maximized', 'fa fa-compress'] :
-    ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
-    return /*#__PURE__*/(
-      React.createElement("div", null, /*#__PURE__*/
-      React.createElement("div", { className: classes[0] }, /*#__PURE__*/
-      React.createElement(Toolbar, {
-        icon: classes[2],
-        onClick: this.handleEditorMaximize,
-        text: "Editor" }), /*#__PURE__*/
+    const classes = this.state.editorMaximized
+      ? ['editorWrap maximized', 'previewWrap hide', 'fa fa-compress']
+      : this.state.previewMaximized
+        ? ['editorWrap hide', 'previewWrap maximized', 'fa fa-compress']
+        : ['editorWrap', 'previewWrap', 'fa fa-arrows-alt'];
 
-      React.createElement(Editor, { markdown: this.state.markdown, onChange: this.handleChange })), /*#__PURE__*/
+    return (
+      <div>
+        <div className={classes[0]}>
+          <Toolbar icon={classes[2]} onClick={this.handleEditorMaximize} text="Editor"/>
+          <Editor markdown={this.state.markdown} onChange={this.handleChange}/>
+        </div>
+        <div className={classes[1]}>
+          <Toolbar icon={classes[2]} onClick={this.handlePreviewMaximize} text="Previewer"/>
+          <Preview markdown={this.state.markdown} onChange={this.handleChange}/>
+        </div>
+      </div>
+    );
+  }
+}
 
-      React.createElement("div", { className: "converter" }), /*#__PURE__*/
-      React.createElement("div", { className: classes[1] }, /*#__PURE__*/
-      React.createElement(Toolbar, {
-        icon: classes[2],
-        onClick: this.handlePreviewMaximize,
-        text: "Previewer" }), /*#__PURE__*/
-
-      React.createElement(Preview, { markdown: this.state.markdown }))));
-
-
-
-  }}
-
-
-const Toolbar = props => {
-  return /*#__PURE__*/(
-    React.createElement("div", { className: "toolbar" }, /*#__PURE__*/
-    React.createElement("i", { className: "fa fa-free-code-camp", title: "no-stack-dub-sack" }),
-    props.text, /*#__PURE__*/
-    React.createElement("i", { className: props.icon, onClick: props.onClick })));
-
-
-};
-
-const Editor = props => {
-  return /*#__PURE__*/(
-    React.createElement("textarea", {
-      id: "editor",
-      onChange: props.onChange,
-      type: "text",
-      value: props.markdown }));
-
-
-};
-
-const Preview = props => {
-  return /*#__PURE__*/(
-    React.createElement("div", {
-      dangerouslySetInnerHTML: {
-        __html: marked(props.markdown, { renderer: renderer }) },
-
-      id: "preview" }));
-
-
-};
-
-// ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('app'));
 export default App;
